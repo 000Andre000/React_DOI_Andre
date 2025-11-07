056f7189b1f40113476db040b57ec466510ee467
000Andre000 <a.fernandes1808@gmail.com>
2025-11-08 01:03:20 +0530

    components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Hero } from './components/Hero.jsx'
+
+
+function App() {
+  return (
+    <>
+    <Hero/>
+    {/* <Explore/>
+    <Goals/>
+    <Experience/>
+    <Vision/>
+    <Footer/> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/icons/ExpressIco.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ExpressIco = ({ className }) => {
+  return (
+    <svg
+      className={className}
+      viewBox="0 0 40 33"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M1.74399 27.5212L19.0862 31.822" stroke="black" strokeWidth="2" />
+      <path d="M2.81039 6.89623L23.9824 22.9585" stroke="black" strokeWidth="2" />
+      <path d="M22.4471 1.01146L29.5835 17.0527" stroke="black" strokeWidth="2" />
+      <path d="M37.1559 3.49137L35.4642 15.1377" stroke="black" strokeWidth="2" />
+    </svg>
+  );
+};
+
+export default ExpressIco;
